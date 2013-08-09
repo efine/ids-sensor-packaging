a9e25c8de1baadd64cc4f54fe94c5b2096363892
Edwin Fine <emofine@usa.net>
2013-08-09 00:57:52 -0400

    Try to ensure that tarballs don't get redownloaded unless necessary

--- debian.mk
+++ debian.mk
@@ -145,7 +145,7 @@ define get_source_template
 	mkdir -p $(DL_DIR)
 	cd $(DL_DIR) && \
 	export dl_tarball=$$(call make_dl_tarball,$(1)) && \
-	if ! test -f $$$$dl_tarball; then $(PGM_CURL) $(CURL_PROXY) --fail -O -s "$$(call make_dl_url,$(1))"; fi && \
+	if ! test -f $$$$dl_tarball; then $(PGM_CURL) $(CURL_PROXY) --fail -O -s "$$(call make_dl_url,$(1))"; else true; fi && \
 	$(PGM_TAR) xfz $$$$dl_tarball && \
 	export tld=`$(PGM_TAR) tfz $$$$dl_tarball | sed 's@/.*$$$$@@' | uniq` && \
 	cp -Rp $$$$tld/* $(PKGDIR)/$(1)/ && \
